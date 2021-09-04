d9aa145db8ad5bec3d5aafe509299b94c8009871
quarkusio <no-reply@quarkus.io>
2021-09-04 17:57:05 +0000

    Initial commit

---
+++ src/main/java/com/github/joostvdg/demo/GreetingConfig.java
@@ -0,0 +1,12 @@
+package com.github.joostvdg.demo;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
